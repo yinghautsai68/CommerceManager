32b7a690c8f75586d2229ca7d57b5ef4f7447c7d
yinghau68 <yinghautsai68cycu@gmail.com>
2026-03-02 21:56:21 +0800

    feat: connect frontend login to backend login API - connect frontend login to backend login API - login and get token!

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -17,16 +17,40 @@ const Login = () => {
         setFormData((prev) => ({ ...prev, [name]: value }));
         console.log(formData);
     }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        try {
+            const response = await fetch(`http://localhost:5000/api/auth/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(formData)
+            })
+            const data = await response.json();
+            if (!response.ok) {
+                return console.log(data.message);
+            }
+            console.log(data.message, data.token);
+            setFormData({
+                username: "",
+                password: ""
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div className='flex flex-col justify-center items-center w-full h-screen'>
-            <form action="" className='flex flex-col justify-between items-center gap-2 w-[80%] h-[50%] px-5 py-10 border  '>
+            <form onSubmit={handleSubmit} className='flex flex-col justify-between items-center gap-2 w-[80%] h-[50%] px-5 py-10 border  '>
                 <FormTitle className='h-[10%] text-center'>Login</FormTitle>
                 <div className='flex flex-col justify-center items-center w-full'>
                     <FormInput name='username' type='text' value={formData.username} handleChange={handleChange}></FormInput>
                     <FormInput name='password' type='password' value={formData.password} handleChange={handleChange}></FormInput>
                 </div>
 
-                <Button className=''>Login!</Button>
+                <Button type='submit' className=''>Login!</Button>
 
             </form>
         </div>
