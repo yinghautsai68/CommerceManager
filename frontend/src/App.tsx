2dd9ac5a4ea2c91a9f12f1b2a46a3fac8229b7eb
yinghau68 <yinghautsai68cycu@gmail.com>
2026-03-02 19:43:13 +0800

    feat: create Login.tsx page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Register from './Pages/Register'
+import Login from './Pages/Login'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/register' element={<Register />} />
-    </Routes>
+      <Route path='/login' element={<Login />} />
+    </Routes >
   )
 }
 
--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { FormTitle } from '../Components/Typography'
+import FormInput from '../Components/FormInput'
+import { Button } from '../Components/Button'
+interface LoginFormDataProps {
+    username: string,
+    password: string
+}
 const Login = () => {
+    const [formData, setFormData] = useState<LoginFormDataProps>({
+        username: "",
+        password: ""
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value }));
+        console.log(formData);
+    }
     return (
-        <div>
-            <form action="">
+        <div className='flex flex-col justify-center items-center w-full h-screen'>
+            <form action="" className='flex flex-col justify-between items-center gap-2 w-[80%] h-[50%] px-5 py-10 border  '>
+                <FormTitle className='h-[10%] text-center'>Login</FormTitle>
+                <div className='flex flex-col justify-center items-center w-full'>
+                    <FormInput name='username' type='text' value={formData.username} handleChange={handleChange}></FormInput>
+                    <FormInput name='password' type='password' value={formData.password} handleChange={handleChange}></FormInput>
+                </div>
+
+                <Button className=''>Login!</Button>
 
             </form>
         </div>
